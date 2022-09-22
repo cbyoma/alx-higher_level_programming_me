86f09c69a37cd38b12a2f73747195200c715ba59
cbyoma <cbyoma@gmail.com>
2022-09-22 04:12:56 -0700

    python script to import add module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-from add_0 import add
-
 if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
 
-    this = add(a, b)
-    print(f"{a} + {b} = {this}")
+    print(f"{a} + {b} = {add(a, b)}")
