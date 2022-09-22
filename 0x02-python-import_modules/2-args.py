b980240e6dd32635d01e18a2bddd7c1d48191c5a
cbyoma <cbyoma@gmail.com>
2022-09-22 04:04:09 -0700

    Python scriptto make a script dynamic!

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    get_number = len(sys.argv) - 1
+
+    if get_number == 0:
+        print("0 arguments.")
+    elif get_number == 1:
+        print("1 argument:")
+    else:
+        print(f"{get_number} arguments:")
+
+    for i in range(get_number):
+        print(f"{i+1}: {sys.argv[i+1]}")
+    
